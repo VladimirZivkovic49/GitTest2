78107a0a308c8915b2f0b581b5abe913fa62f39d
VladaZSource <vlaja432@gmail.com>
2025-05-11 18:37:28 +0200

    Commit 1 dodata klasa Solution

---
+++ TestGitIgnore/TestGitIgnore/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGitIgnore
+{
+    public class Solution
+    {
+        public Solution(int a, int b)
+        {
+            Suma = func(a, b);
+        }
+
+        public int Suma { get; }
+
+        private int func(int a, int b)
+        {
+            return (a + b);
+
+        }
+
+
+
+    }
+}
